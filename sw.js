f11952db497b82f3b2c9b0addf79817b77ef8636
Mi3s <maitreeable@gmail.com>
2026-07-22 08:56:34 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: network-first for world.json (fresh data when online, cached
 // world when roaming without signal), cache-first for everything else.
 
-const CACHE = "mirage-v12";
+const CACHE = "mirage-v13";
 const SHELL = [
   "./",
   "./index.html",
